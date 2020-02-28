88d503e69b98e38fcd5752a1ee53913d9b02e2e9
Kevin Poston <kposton@qualtrax.com>
2020-02-28 14:32:34 -0500

    Added tests for roll result

---
+++ MonopolyTests/RollResultTests.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Monopoly;
+
+namespace MonopolyTests
+{
+    [TestClass]
+    public class RollResultTests
+    {
+        [TestMethod]
+        public void TotalReturnsTheSumOfBothRolls()
+        {
+            var expectedTotal = 9;
+            var rollResult = new RollResult(5, 4);
+
+            Assert.AreEqual(expectedTotal, rollResult.Total);
+        }
+
+        [TestMethod]
+        public void IsDoublesReturnsTrueWhenRollsAreEqual()
+        {
+            var roll = new RollResult(5, 5);
+            Assert.IsTrue(roll.IsDoubles);
+        }
+
+        [TestMethod]
+        public void IsDoublesReturnsFalseWhenRollsAreNotEqual()
+        {
+            var roll = new RollResult(5, 4);
+            Assert.IsFalse(roll.IsDoubles);
+        }
+    }
+}
