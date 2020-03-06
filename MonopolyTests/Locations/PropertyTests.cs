d2c52a971b5dc65d85017ace277fb7f3d33f38f6
Kevin Poston <kposton@qualtrax.com>
2020-03-06 16:16:14 -0500

    Added property tests and finished R3 User Story 5

---
+++ MonopolyTests/Locations/PropertyTests.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Monopoly.Locations;
+
+namespace MonopolyTests.Locations
+{
+    [TestClass]
+    public class PropertyTests
+    {
+        private IProperty property;
+
+        public PropertyTests()
+        {
+            property = new Property(LocationConstants.MediterraneanAveIndex, LocationConstants.PurplePropertyGroup, null, LocationConstants.MediterraneanAveCost, LocationConstants.MediterraneanAveRent);
+        }
+
+        [TestMethod]
+        public void MortgagePropertySetsIsMortgagedToTrue()
+        {
+            property.MortgageProperty();
+
+            Assert.IsTrue(property.IsMortgaged);
+        }
+
+        [TestMethod]
+        public void UnmortgagePropertySetsIsMortgagedToFalse()
+        {
+            property.UnmortgageProperty();
+
+            Assert.IsFalse(property.IsMortgaged);
+        }
+    }
+}
