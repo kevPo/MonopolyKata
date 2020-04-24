9b84f7d52c8e470bc0cfff88c9a7513340605d35
Tripp Shelnutt <shelnutt77@gmail.com>
2020-04-24 13:28:08 -0400

    Adjusted namespace for LinqExtensions

--- Monopoly/LinqExtensions.cs
+++ Monopoly/LinqExtensions.cs
@@ -1,6 +1,8 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
-namespace System.Linq
+namespace Monopoly
 {
     public static class LinqExtensions
     {
